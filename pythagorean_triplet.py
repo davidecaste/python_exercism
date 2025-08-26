41cc2a0b56131418b109d64db708d924bc6acf14
davidecaste <castelnovod@gmail.com>
2025-08-26 16:19:43 +0200

    add

---
+++ pythagorean_triplet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def triplets_with_sum(n):
+    trip = []
+    for a in range(1, int(n/3)+1):
+        c = (n**2 - 2*a*n + 2*a**2) / (2*(n - a))
+        b = n - a - int(c)
+        if (not (c).is_integer()) or b <= a:
+            print(c)
+            continue 
+        trip.append([a,b,int(c)])
+    return trip 
