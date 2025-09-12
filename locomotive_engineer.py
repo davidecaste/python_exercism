e48762dd7cd59caab178f91259a82147e4c10c0b
davidecaste <castelnovod@gmail.com>
2025-09-12 17:48:11 +0200

    minori

---
+++ locomotive_engineer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def get_list_of_wagons(*args):
+    return args
--- word_count.py
+++ word_count.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 
-import string 
+import re
 def count_words(sentence):
-    PUNCT = (string.punctuation + string.whitespace).replace("'", "")
-    for p in PUNCT:
-        sentence = sentence.replace(p, ' ')
-    dict = {} 
-    words = sentence.split()
-    for word in words:
-        if word not in dict.keys():
-            dict(word) = 1
-        else:
-            dict(word) += 1
-    return dict 
+    words = re.findall(r"[a-z\d]+'?[a-z\d]+|\d+|[a-z]", sentence.lower())
+    return {word: words.count(word) for word in words}
